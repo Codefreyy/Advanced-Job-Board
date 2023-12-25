37243a93cad34df070274ca0b3169c8ec11aa3d0
Joy <joyyujiepeng@gmail.com>
2023-12-25 16:16:11 +0000

    feat: set up base api

---
+++ client/src/services/baseApi.ts
@@ -0,0 +1,16 @@
+import { env } from "@/constants/config";
+import axios from "axios";
+
+export const baseApi = axios.create({
+    baseURL: env.VITE_API_URL,
+    // pass the cookie up
+    withCredentials: true
+})
+
+if (env.VITE_TEST_SLOW_API) {
+    baseApi.interceptors.request.use(req => {
+        return new Promise(resolve => {
+            setTimeout(resolve, 2000)
+        })
+    }
+}
