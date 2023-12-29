4f5fa89dffc254f2932fb0978c8fb0e6b6780ccd
Joy <joyyujiepeng@gmail.com>
2023-12-29 22:44:58 +0000

    feat: implement delete job listing

--- client/src/features/job-listing/components/MyJobListingGrid.tsx
+++ client/src/features/job-listing/components/MyJobListingGrid.tsx
@@ -1,21 +1,44 @@
 import { Button } from "@/components/ui/button"
+import { deleteListing } from "@/features/job-listing/services/jobs"
+import { useState } from "react"
 import { Link } from "react-router-dom"
 import { JobListing } from "../constants/types"
-import { deleteListing } from "../services/jobs"
-import DeleteJobListingDialog from "./DeleteJobListingDialog"
 import JobListingCard from "./JobListingCard"
 import JobListingGrid from "./JobListingGrid"
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog"
 
 type MyJobListingGridProps = {
   jobListings: JobListing[]
 }
 
 const MyJobListingGrid = ({ jobListings }: MyJobListingGridProps) => {
-  console.log("lsiting", jobListings)
+  const [deletedJobListingIds, setDeletedJobListingIds] = useState<string[]>([])
+
+  const visibleJobListing = jobListings.filter(
+    (jobListing) => !deletedJobListingIds.includes(jobListing.id)
+  )
+
+  async function deleteJobListing(deletedId: string) {
+    await deleteListing(deletedId)
+    setDeletedJobListingIds((ids) => [...ids, deletedId])
+  }
   return (
     <JobListingGrid>
-      {jobListings.map((job) => (
-        <MyJobListingCard jobListing={job} key={job.id} />
+      {visibleJobListing.map((job) => (
+        <MyJobListingCard
+          jobListing={job}
+          key={job.id}
+          deleteJobListing={deleteJobListing}
+        />
       ))}
     </JobListingGrid>
   )
@@ -23,26 +46,56 @@ const MyJobListingGrid = ({ jobListings }: MyJobListingGridProps) => {
 
 type MyJobListingCardProps = {
   jobListing: JobListing
+  deleteJobListing: (id: string) => void
 }
 
-function MyJobListingCard({ jobListing }: MyJobListingCardProps) {
-  async function handleConfirm() {
-    console.log(jobListing.id)
-    await deleteListing(jobListing.id)
-  }
-
+function MyJobListingCard({
+  jobListing,
+  deleteJobListing,
+}: MyJobListingCardProps) {
   return (
     <JobListingCard
       job={jobListing}
       footerBtns={
         <>
-          <DeleteJobListingDialog deleteListing={handleConfirm} />
+          <DeleteJobListingDialog
+            deleteListing={() => deleteJobListing(jobListing.id)}
+          />
           <Button variant="outline">
             <Link to={`/jobs/${jobListing.id}/edit`}>Edit</Link>
           </Button>
         </>
       }
     />
+  )
+}
+
+const DeleteJobListingDialog = ({
+  deleteListing,
+}: {
+  deleteListing: (id: string) => void
+}) => {
+  return (
+    <AlertDialog>
+      <AlertDialogTrigger>
+        <Button variant="ghost">Delete</Button>
+      </AlertDialogTrigger>
+      <AlertDialogContent>
+        <AlertDialogHeader>
+          Are you sure you want to delete this job listing?
+        </AlertDialogHeader>
+        <AlertDialogDescription>
+          This action cannot be undone. This will permanently delete your job
+          listing and any remaining time will not be refunded.
+        </AlertDialogDescription>
+        <AlertDialogFooter>
+          <AlertDialogCancel>Cancel</AlertDialogCancel>
+          <AlertDialogAction onClick={deleteListing}>
+            Continue
+          </AlertDialogAction>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
   )
 }
 
